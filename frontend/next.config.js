80f0985a25a6758f7046bf5de141b907e7d75046
Zyk <basesukid@gmail.com>
2026-08-31 18:17:54 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -16,11 +16,25 @@ module.exports = {
         }),
     },
     exportPathMap: async function (defaultPathMap) {
+        // Remove all dynamic routes
         Object.keys(defaultPathMap).forEach(path => {
             if (path.includes('[') && path.includes(']')) {
                 delete defaultPathMap[path];
             }
         });
+        
+        // Remove pages with getInitialProps that fail during export
+        const excludePages = [
+            '/Forum/ShowPost.aspx',
+            '/User.aspx',
+            '/develop',
+            '/donate',
+            '/download',
+            '/Catalog.aspx',
+            '/trades',
+        ];
+        excludePages.forEach(p => delete defaultPathMap[p]);
+        
         return defaultPathMap;
     },
 };
