152045db41021a216295209c618342aecd88e01a
Zyk <basesukid@gmail.com>
2026-08-31 17:55:36 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -25,6 +25,14 @@ module.exports = {
     images: { unoptimized: true },
     trailingSlash: true,
     reactStrictMode: true,
+    publicRuntimeConfig: {
+        backend: {
+            baseUrl: '',
+            apiFormat: '/apisite/{0}{1}',
+            proxyEnabled: false,
+            flags: {}
+        }
+    },
     env: {
         NEXT_PUBLIC_KORONE_PUBLIC_CONFIG: JSON.stringify({
             publicRuntimeConfig: {
